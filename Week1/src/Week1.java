fe0c4c7d21f0f1067bf6224756861ed017f1c6ef
Plamen Neshkov <plamen0neshkov@gmail.com>
2015-11-23 18:50:55 +0200

    Finish week1

--- Week1/src/Week1.java
+++ Week1/src/Week1.java
@@ -1,7 +1,6 @@
 public class Week1 {
     public static void main(String[] args) {
-    	int[] numbers = new int[] {2, -1, 2, 2, -1, 2, 2, 2};
-    	System.out.println(Task38.zeroInsert(1));
+    	//test tasks here
     }
 }
 
