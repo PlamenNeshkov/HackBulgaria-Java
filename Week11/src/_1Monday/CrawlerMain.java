6e822560172f7c0f69f90177de32c17391ed8b69
Plamen Neshkov <plamen0neshkov@gmail.com>
2016-02-09 15:45:24 +0200

    Remove throws declaration from main

--- Week11/src/_1Monday/CrawlerMain.java
+++ Week11/src/_1Monday/CrawlerMain.java
@@ -7,8 +7,12 @@ import java.io.IOException;
  * Created by plamen on 2/8/16.
  */
 public class CrawlerMain {
-    public static void main(String[] args) throws IOException {
-        Crawler crawler = new Crawler(args[0], args[1]);
-        crawler.start();
+    public static void main(String[] args) {
+        try {
+            Crawler crawler = new Crawler(args[0], args[1]);
+            crawler.start();
+        } catch (IOException e) {
+            System.err.println("I/O error");
+        }
     }
 }
